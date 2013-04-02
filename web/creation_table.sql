be2fead9c26c8273066b2b9de130d06b01471304
calbrix <pierro12345@hotmail.fr>
2013-04-02 10:31:40 +0200

    Modification de la base de données

--- web/creation_table.sql
+++ web/creation_table.sql
@@ -6,6 +6,7 @@ DROP TABLE ALUMNI.ENTREPRISE;
 DROP TABLE ALUMNI.COMPTE;
 DROP TABLE ALUMNI.PROMOTION;
 DROP TABLE ALUMNI.POSTE;
+DROP TABLE ALUMNI.RELATIONETUDIANT
 
 
 
@@ -34,15 +35,16 @@ CREATE TABLE ALUMNI.ENTREPRISE(
 CREATE TABLE ALUMNI.PROMOTION(
 	idPromotion             INT NOT NULL GENERATED ALWAYS AS IDENTITY(START WITH 1, INCREMENT BY 1),
 	annee 			DATE NOT NULL, 
-	type 			VARCHAR(20) NOT NULL,
+	typePromo 		VARCHAR(20) CHECK (typePromo IN('CLASSIQUE','APPRENTISSAGE')),
 	PRIMARY KEY(idPromotion)
 );
 
 CREATE TABLE ALUMNI.POSTE(
 	idPoste 		INT NOT NULL GENERATED ALWAYS AS IDENTITY(START WITH 1, INCREMENT BY 1),
-	idEntreprise            INT, 
 	intitule		VARCHAR(30),
 	description		VARCHAR(255),
+        localisation            VARCHAR(10) NOT NULL CHECK (localisation IN('FRANCE','AUTRES')) DEFAULT 'FRANCE',   
+        dateDebut               Date,
 	salaire			DOUBLE,
 	PRIMARY KEY(idPoste)
 );
@@ -50,50 +52,58 @@ CREATE TABLE ALUMNI.POSTE(
 CREATE TABLE ALUMNI.ETUDIANT(
 	idEtudiant		INT NOT NULL GENERATED ALWAYS AS IDENTITY(START WITH 1, INCREMENT BY 1),
 	idPromotion		INT,
-	idCompte		INT NOT NULL,
+	idCompte		INT NOT NULL, 
+        idPoste                 INT,
+        genre                   VARCHAR(7) CHECK (genre IN('HOMME','FEMME')),
+        dateDeNaissance         DATE,
 	nom 			VARCHAR(30)NOT NULL,
 	prenom 			VARCHAR(30)NOT NULL,
 	adresse 		VARCHAR(255)NOT NULL,
 	mail			VARCHAR(30)NOT NULL,
-	tel			INTEGER,      
-PRIMARY KEY (idEtudiant),
-FOREIGN KEY (idPromotion) REFERENCES ALUMNI.PROMOTION,
-FOREIGN KEY (idCompte) REFERENCES ALUMNI.COMPTE
+	tel			VARCHAR(15),      
+        photoProfil             VARCHAR(20),
+        CV                      VARCHAR(20),
+        PRIMARY KEY (idEtudiant),
+        FOREIGN KEY (idCompte) REFERENCES ALUMNI.COMPTE
+);
+
+CREATE TABLE ALUMNI.RELATIONETUDIANT(
+        idEtudiant1 INT NOT NULL,
+        idEtudiant2 INT NOT NULL,
+        PRIMARY KEY (idEtudiant1, idEtudiant2),
+        FOREIGN KEY (idEtudiant1) REFERENCES ALUMNI.ETUDIANT,
+        FOREIGN KEY (idEtudiant2) REFERENCES ALUMNI.ETUDIANT
 );
 
 CREATE TABLE ALUMNI.HISTORIQUE_ETUDIANT_POSTE(
-	idEtudiant 		INT NOT NULL,
-	idPoste			INT NOT NULL, 
-	dateDebut		DATE NOT NULL,
-	dateFin			DATE NOT NULL,
-	salaire			INT NOT NULL,
-	localisation            VARCHAR(30) NOT NULL,
-	categorie		VARCHAR(10) NOT NULL,
+	idEtudiant 		INT,
+	idPoste			INT, 
+	dateFin			DATE,
 	FOREIGN KEY(idEtudiant) REFERENCES ALUMNI.Etudiant(idEtudiant),
 	FOREIGN KEY(idPoste) REFERENCES ALUMNI.Poste(idPoste),
 	PRIMARY KEY(idEtudiant, idPoste)
 );
 
 CREATE TABLE ALUMNI.RESPONSABLE(
-idInscrit		INT NOT NULL GENERATED ALWAYS AS IDENTITY(START WITH 1, INCREMENT BY 1),
-idCompte		INT NOT NULL,
-numHarpege		INTEGER,
-nom 			VARCHAR(30) NOT NULL,
-prenom 			VARCHAR(30) NOT NULL,
-adresese 		VARCHAR(255) NOT NULL,
-mail			VARCHAR(30) NOT NULL,
-tel			INTEGER,      
-PRIMARY KEY (idInscrit),
-FOREIGN KEY (idCompte) REFERENCES ALUMNI.COMPTE
+        idInscrit		INT NOT NULL GENERATED ALWAYS AS IDENTITY(START WITH 1, INCREMENT BY 1),
+        idCompte		INT NOT NULL,
+        numHarpege		VARCHAR(15),
+        nom 			VARCHAR(30) NOT NULL,
+        prenom 			VARCHAR(30) NOT NULL,
+        adresse 		VARCHAR(255) NOT NULL,
+        mail			VARCHAR(30) NOT NULL,
+        tel			INTEGER,      
+        PRIMARY KEY (idInscrit),
+        FOREIGN KEY (idCompte) REFERENCES ALUMNI.COMPTE
 );
 
 CREATE TABLE ALUMNI.MESSAGE(
-idMessage                   INT NOT NULL GENERATED ALWAYS AS IDENTITY(START WITH 1, INCREMENT BY 1),
-idCompteEmmetteur           INT NOT NULL,
-idCompteDestinataire        INT NOT NULL,
-date                        DATE,
-message                     VARCHAR(255)NOT NULL,    
-PRIMARY KEY (idMessage),
-FOREIGN KEY (idCompteEmmetteur) REFERENCES ALUMNI.COMPTE,
-FOREIGN KEY (idCompteDestinataire) REFERENCES ALUMNI.COMPTE
+        idMessage                   INT NOT NULL GENERATED ALWAYS AS IDENTITY(START WITH 1, INCREMENT BY 1),
+        idCompteEmmetteur           INT NOT NULL,
+        idCompteDestinataire        INT NOT NULL,
+        dateEnvoie                  DATE,
+        message                     VARCHAR(255)NOT NULL,    
+        PRIMARY KEY (idMessage),
+        FOREIGN KEY (idCompteEmmetteur) REFERENCES ALUMNI.COMPTE,
+        FOREIGN KEY (idCompteDestinataire) REFERENCES ALUMNI.COMPTE
 );
